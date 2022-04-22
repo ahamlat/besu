898ce9288d1824dd3d59550b2285d16e86235a6a
Ameziane H <ameziane.hamlat@consensys.net>
2022-04-22 18:02:10 +0200

    Performance test : cache executed transactions Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/ReceiptsCache.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/ReceiptsCache.java
@@ -4,15 +4,11 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.ethereum.core.TransactionReceipt;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.List;
 
 public class ReceiptsCache {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ReceiptsCache.class);
 
     private final Cache<Hash, List<TransactionReceipt>> cache;
 
@@ -24,12 +20,10 @@ public class ReceiptsCache {
     }
 
     public List<TransactionReceipt> getIfPresent(final Hash codeHash) {
-        LOG.info("ReceiptsCache - getting "+codeHash+" from the cache");
         return cache.getIfPresent(codeHash);
     }
 
     public void put(final Hash key, final List<TransactionReceipt> value) {
-        LOG.info("ReceiptsCache - putting "+key+" in the cache");
         cache.put(key, value);
 
     }
