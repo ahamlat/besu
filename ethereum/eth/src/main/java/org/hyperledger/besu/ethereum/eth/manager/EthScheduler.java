ef86fb40c34515866d7327390ca09a23cd6b9414
Ameziane H. <ameziane.hamlat@consensys.net>
2025-05-05 16:24:14 +0200

    Create a real fixed Executor
    Signed-off-by: Ameziane H. <ameziane.hamlat@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthScheduler.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthScheduler.java
@@ -76,7 +76,7 @@ public class EthScheduler {
       final MetricsSystem metricsSystem) {
     this(
         MonitoredExecutors.newFixedThreadPool(
-            EthScheduler.class.getSimpleName() + "-Workers", 1, syncWorkerCount, metricsSystem),
+            EthScheduler.class.getSimpleName() + "-Workers", syncWorkerCount, syncWorkerCount, metricsSystem),
         MonitoredExecutors.newScheduledThreadPool(
             EthScheduler.class.getSimpleName() + "-Timer", 1, metricsSystem),
         MonitoredExecutors.newBoundedThreadPool(
